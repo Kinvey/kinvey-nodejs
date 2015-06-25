f3b03a63e70955ca311d9252c69a6a5d5e0b91cb
Thomas Conner <thomas.conner@me.com>
2015-06-25 07:15:37 -0400

    Updated to 1.4.0
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

--- kinvey.js
+++ kinvey.js
@@ -115,7 +115,7 @@
      * @type {string}
      * @default
      */
-    Kinvey.SDK_VERSION = '1.3.6';
+    Kinvey.SDK_VERSION = '1.4.0';
 
     // Properties.
     // -----------
@@ -380,7 +380,7 @@
         // Initialize the synchronization namespace and restore the active user.
         return Kinvey.Sync.init(options.sync);
       }).then(function() {
-        log('Kinvey initialized, running version: js-nodejs/1.3.6');
+        log('Kinvey initialized, running version: js-nodejs/1.4.0');
         return restoreActiveUser(options);
       });
 
@@ -1758,7 +1758,7 @@
       }
 
       // Return the device information string.
-      var parts = ['js-nodejs/1.3.6'];
+      var parts = ['js-nodejs/1.4.0'];
       if(0 !== libraries.length) { // Add external library information.
         parts.push('(' + libraries.sort().join(', ') + ')');
       }
