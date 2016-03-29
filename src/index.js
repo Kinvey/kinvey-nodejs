d565e3c894d61b64fd554759eb6c9ec3ad0dd8f7
Thomas Conner <thomas@kinvey.com>
2016-03-29 09:46:32 -0400

    Bump version to 3.0.0-beta.12

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-import { Kinvey } from 'kinvey-sdk-core';
+import { Kinvey } from 'kinvey-javascript-sdk-core';
+import { NetworkRack } from 'kinvey-javascript-sdk-core/src/rack/rack';
+import { SerializeMiddleware } from 'kinvey-javascript-sdk-core/src/rack/middleware/serialize';
+import { HttpMiddleware } from './http';
+import { Device } from 'kinvey-javascript-sdk-core/src/utils/device';
+import { DeviceAdapter } from './device';
+
+// Add Http middleware
+const networkRack = NetworkRack.sharedInstance();
+networkRack.useAfter(SerializeMiddleware, new HttpMiddleware());
+
+// Use Device Adapter
+Device.use(new DeviceAdapter());
+
+// Export
 module.exports = Kinvey;
